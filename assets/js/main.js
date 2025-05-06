ae9d171dc8d1f66d15b11aff7615b206b6a514e4
sreyasilp <er.sreyas.tp@gmail.com>
2025-05-06 16:52:24 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Imperial
-* Template URL: https://bootstrapmade.com/imperial-free-onepage-bootstrap-theme/
-* Updated: Oct 08 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
